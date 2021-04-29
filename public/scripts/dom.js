1df23103674b369459f71ef01bec8515e6cc303a
Jhon Abril <>
2021-04-29 02:23:41 +0000

    Some file names changed

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded',() => {
     restart.classList.remove("hide");
     restart.classList.add("show");
     Mundo=update(Mundo,{play:1, dir:{x:0,y:0}});
-  })
+  });
   const instructionsButton = document.getElementById('buttonInstructions');
   const instructionsModal = document.getElementById("instructions");
   instructionsButton.addEventListener('click',() => {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded',() => {
     playModal.classList.add("show");
     instructionsModal.classList.remove("show");
     instructionsModal.classList.add("hide");
-  })
+  });
   const contributorsButton = document.getElementById("contributorsButton");
   const contributorsModal = document.getElementById("contributors")
   contributorsButton.addEventListener('click',() => {
@@ -33,14 +33,14 @@ document.addEventListener('DOMContentLoaded',() => {
     playModal.classList.add("hide");
     contributorsModal.classList.remove("hide");
     contributorsModal.classList.add("show");
-  })
+  });
   const backButtonCon = document.getElementById("backCon");
   backButtonCon.addEventListener('click',() => {
     playModal.classList.remove("hide");
     playModal.classList.add("show");
     contributorsModal.classList.remove("show");
     contributorsModal.classList.add("hide");
-  })
+  });
   const scoresButton = document.getElementById("scoresButton");
   const scoresModal = document.getElementById("scoresModal")
   scoresButton.addEventListener('click',() => {
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded',() => {
     playModal.classList.add("hide");
     scoresModal.classList.remove("hide");
     scoresModal.classList.add("show");
-  })
+  });
   const backButtonScore = document.getElementById("backScore");
   backButtonScore.addEventListener('click',() => {
     playModal.classList.remove("hide");
@@ -63,13 +63,26 @@ document.addEventListener('DOMContentLoaded',() => {
     scoresModal.classList.add("hide");
     graphics.classList.remove("hide");
     graphics.classList.add("show");
-  })
+  });
   const backGraficsButton = document.getElementById("backGraficsButton");
   backGraficsButton.addEventListener("click", () => {
     scoresModal.classList.remove("hide");
     scoresModal.classList.add("show");
     graphics.classList.remove("show");
     graphics.classList.add("hide");
-  })
-  
-})
+  });
+});
+
+// Eventos del mouse y teclado
+
+addEventListener("contextmenu", event => event.preventDefault());
+
+addEventListener("keydown", event => {
+  if(event.ctrlKey && event.key == "u") event.preventDefault();
+  if(event.ctrlKey && event.key == "s") event.preventDefault();
+  if(event.ctrlKey && event.key == "p") event.preventDefault();
+  if(event.ctrlKey && event.key == "c") event.preventDefault();
+  if(event.ctrlKey && event.path.shiftKey && event.key == "i") event.preventDefault();
+  if(event.ctrlKey && event.shiftKey && (event.key == "i" || event.key == "I")) event.preventDefault();
+  if(event.ctrlKey && event.shiftKey && (event.key == "j" || event.key == "J")) event.preventDefault();
+});
